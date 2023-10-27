b709a5128960aa9e946b1c1f9e283abaaef25b70
Martin Kristof <martin.kristof@heureka.cz>
2023-10-27 14:01:37 +0200

    fix: fix container for mobile

--- src/app/_components/ui/Container.tsx
+++ src/app/_components/ui/Container.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from 'react';
 
 export const Container: FC<{ children: ReactNode }> = ({ children }) => (
-  <section className="py-3 container mx-auto">{children}</section>
+  <section className="py-3 container mx-auto px-4">{children}</section>
 );
