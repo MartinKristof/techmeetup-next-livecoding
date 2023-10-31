d6721e7261c53b83b227a3731becc9732f3bdce2
Martin Kristof <martin.kristof@heureka.cz>
2023-10-31 20:05:27 +0100

    feat: add pending status for SubmitButton

--- src/app/_components/ui/SubmitButton.tsx
+++ src/app/_components/ui/SubmitButton.tsx
@@ -1,7 +1,17 @@
 import { FC, ReactNode } from 'react';
+import { useFormStatus } from 'react-dom';
 
-export const SubmitButton: FC<{ children: ReactNode }> = ({ children }) => (
-  <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
-    {children}
-  </button>
-);
+export const SubmitButton: FC<{ children: ReactNode }> = ({ children }) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      className={`bg-green-600 font-bold text-white py-3 px-6 w-fit ${pending ? 'disabled:opacity-50' : ''}`}
+      aria-disabled={pending}
+      disabled={pending}
+    >
+      {children}
+    </button>
+  );
+};
